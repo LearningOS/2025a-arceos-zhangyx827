fd02177ede19dd3fbe84d9fe8f1ee40375e59e6c
zyx <zhangyx@gmail.com>
2025-11-03 21:07:18 +0800

    update

--- arceos/modules/bump_allocator/src/lib.rs
+++ arceos/modules/bump_allocator/src/lib.rs
@@ -1,5 +1,9 @@
 #![no_std]
 
+use core::ptr::NonNull;
+// use core::alloc::AllocError;
+use allocator::AllocError;
+
 use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 
 /// Early memory allocator
@@ -16,17 +20,43 @@ use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 /// When it goes down to ZERO, free bytes-used area.
 /// For pages area, it will never be freed!
 ///
-pub struct EarlyAllocator<const SIZE: usize> {}
+pub struct EarlyAllocator<const SIZE: usize> {
+    start: usize,
+    b_pos: usize,
+    b_end: usize,
+    p_pos: usize,
+    end: usize,
+    total_bytes: usize,
+    used_bytes: usize,
+    total_pages: usize,
+    used_pages: usize,
+}
 
 impl<const SIZE: usize> EarlyAllocator<SIZE> {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            start: 0,
+            b_pos: 0,
+            end: 0,
+            p_pos: 0,
+            b_end: 0,
+            used_bytes: 0,
+            total_bytes: 0,
+            total_pages: 0,
+            used_pages: 0,
+        }
     }
 }
 
 impl<const SIZE: usize> BaseAllocator for EarlyAllocator<SIZE> {
     fn init(&mut self, start: usize, size: usize) {
-        todo!()
+        self.start = start;
+        self.b_pos = start;
+        self.end = start + size;
+        self.p_pos = start + size;
+        self.used_bytes = 0;
+        self.total_bytes = size;
+        self.total_pages = size / SIZE;
     }
 
     fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
@@ -39,23 +69,33 @@ impl<const SIZE: usize> ByteAllocator for EarlyAllocator<SIZE> {
         &mut self,
         layout: core::alloc::Layout,
     ) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
-        todo!()
+        if self.b_pos + layout.size() > self.p_pos {
+            return Err(AllocError::NoMemory);
+        }
+        unsafe {
+            let ptr = NonNull::new_unchecked(self.b_pos as *mut u8);
+            self.used_bytes += layout.size();
+            self.total_bytes -= layout.size();
+            self.total_pages -= layout.size() / SIZE;
+            Ok(ptr)
+        }
     }
 
     fn dealloc(&mut self, pos: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
-        todo!()
+        // todo!()
+        
     }
 
     fn total_bytes(&self) -> usize {
-        todo!()
+        self.total_bytes
     }
 
     fn used_bytes(&self) -> usize {
-        todo!()
+        self.used_bytes
     }
 
     fn available_bytes(&self) -> usize {
-        todo!()
+        self.total_bytes - self.used_bytes
     }
 }
 
@@ -67,7 +107,24 @@ impl<const SIZE: usize> PageAllocator for EarlyAllocator<SIZE> {
         num_pages: usize,
         align_pow2: usize,
     ) -> allocator::AllocResult<usize> {
-        todo!()
+        if align_pow2 % Self::PAGE_SIZE != 0 {
+            return Err(AllocError::InvalidParam);
+        }
+        let align_pow2 = align_pow2 / SIZE;
+        if !align_pow2.is_power_of_two() {
+            return Err(AllocError::InvalidParam);
+        }
+        if self.p_pos - num_pages * SIZE < self.b_pos {
+            return Err(AllocError::NoMemory);
+        }
+
+
+        self.p_pos -= num_pages * SIZE;
+        self.total_pages += num_pages;
+        self.used_pages += num_pages;
+        self.total_bytes -= num_pages * SIZE;
+        self.total_pages -= num_pages;
+        Ok(self.p_pos)
     }
 
     fn dealloc_pages(&mut self, pos: usize, num_pages: usize) {
@@ -75,14 +132,14 @@ impl<const SIZE: usize> PageAllocator for EarlyAllocator<SIZE> {
     }
 
     fn total_pages(&self) -> usize {
-        todo!()
+        self.total_pages
     }
 
     fn used_pages(&self) -> usize {
-        todo!()
+        self.used_pages
     }
 
     fn available_pages(&self) -> usize {
-        todo!()
+        self.total_pages - self.used_pages
     }
 }
---
+++ arceos/ulib/axstd/src/collections.rs
@@ -0,0 +1,91 @@
+#![cfg(feature = "alloc")]
+use axhal::misc::random;
+use xxhash_rust::const_xxh3::xxh3_64 as const_xxh3;
+use alloc::boxed::Box;
+
+const TABLE_SIZE: usize = 50101;
+pub struct HashMap<K, V> {
+    pairs: Box<[Option<(K, V)>]>,
+    step: u128, // a random probe step
+}
+
+impl<K, V> HashMap<K, V> {
+    pub fn hash(key: &K) -> u64 {
+        unsafe {
+            let input = core::slice::from_raw_parts(key as *const K as *const u8, core::mem::size_of::<K>());
+            const_xxh3(input) % (TABLE_SIZE as u64)
+        }
+    }
+
+    pub fn new() -> Self {
+        HashMap {  
+            pairs: Box::new([const {None}; TABLE_SIZE]),
+            step: random() % TABLE_SIZE as u128,
+        }
+    }
+
+    pub fn insert(&mut self, key: K, value: V) {
+        let mut ind = Self::hash(&key);
+
+        while let Some(_value) = &self.pairs[ind as usize] {
+            ind = (ind + self.step as u64) % TABLE_SIZE as u64;
+        }
+
+        self.pairs[ind as usize] = Some((key, value));
+    }
+
+    pub fn iter(&self) -> HashMapIter<K, V> {
+        HashMapIter { pairs: &self.pairs, cur_pos: 0 }
+    }
+}
+
+pub struct HashMapIterator<K, V> {
+    pairs: Box<[Option<(K, V)>]>,
+    cur_pos: usize,
+}
+
+impl<K, V> IntoIterator for HashMap<K, V> {
+    type Item = (K, V);
+    type IntoIter = HashMapIterator<K, V>;
+    fn into_iter(self) -> Self::IntoIter {
+        HashMapIterator {
+            pairs: self.pairs,
+            cur_pos: 0,
+        }
+    }
+}
+
+impl<K, V> Iterator for HashMapIterator<K, V> {
+    type Item = (K, V);
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.cur_pos < TABLE_SIZE {
+            if let Some(pair) = self.pairs[self.cur_pos].take() {
+                self.cur_pos += 1;
+                return Some(pair);
+            }
+
+            self.cur_pos += 1;
+        }
+
+        return None;
+    }
+}
+
+pub struct HashMapIter<'a, K: 'a, V: 'a> {
+    pairs: &'a [Option<(K, V)>],
+    cur_pos: usize,
+}
+
+impl<'a, K, V> Iterator for HashMapIter<'a, K, V> {
+    type Item = (&'a K, &'a V);
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.cur_pos < TABLE_SIZE {
+            if let Some(pair) = &self.pairs[self.cur_pos] {
+                self.cur_pos += 1;
+                return Some((&pair.0, &pair.1));
+            }
+            self.cur_pos += 1;
+        }
+        return None;
+    }
+}
